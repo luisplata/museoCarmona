5391810c6b2024b82a0f53cd1e86c01d1338462b
Luis Plata <www.luisplata@gmail.com>
2021-07-24 16:31:28 -0500

    in service is modifed for new presentation

--- resources/views/components/home/services.blade.php
+++ resources/views/components/home/services.blade.php
@@ -42,4 +42,49 @@
             <img class="image" src="{{asset($webInfo[3]->img)}}" alt="">
         </div>
     </div>
+    <div class="">
+        <div class="row rowOne">
+            <div class="col s12">
+                <div class="card">
+                    <div class="card-image">
+                    <img src="uploads/2/2020-10/plaza_san_pedro_claver_4.jpg">
+                    <span class="card-title">
+                        {{$webInfo[3]->title}}
+                        <span>
+                            {{$webInfo[3]->subtitle}}
+                        </span>
+                        <hr>
+                    </span>
+                    </div>
+                    <div class="card-content">
+                        <div class="rowTwo rowBig">
+
+                        @foreach($services as $s)
+                            <div class="column col s3">
+                                <div class="rowTwo">
+                                    <div class="medium col">
+                                        <div class="img">
+                                            <div>
+                                                <img src="{{asset($s->img)}}" alt="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="medium_2 col">
+                                        <h2>
+                                            {{$s->name}}
+                                        </h2>
+                                        <h3>
+                                            {{$s->text}}
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
