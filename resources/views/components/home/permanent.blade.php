393d8cf58b963f606e1c19f0e92b578a6afd0df2
Luis Plata <www.luisplata@gmail.com>
2021-08-21 14:57:41 -0500

    update css fot the works permanents

--- resources/views/components/home/permanent.blade.php
+++ resources/views/components/home/permanent.blade.php
@@ -11,7 +11,7 @@
                 @foreach($works as $w)
                     <div>
                           <div class="row rowTwo">
-                              <div class="col s12 m8 l8">
+                              <div class="col s12 m8 l8 imgcenter">
                                   <img src="{{asset($w->img)}}" alt="">
                               </div>
                               <div class="col s12 m4 l4">
